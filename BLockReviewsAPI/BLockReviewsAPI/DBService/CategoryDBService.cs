a10e12077080176fbd39bb9060bb455503f2cf7d
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-03-18 13:24:49 +0900

    모델 변경, 카테고리 조회

---
+++ BLockReviewsAPI/BLockReviewsAPI/DBService/CategoryDBService.cs
@@ -0,0 +1,35 @@
+﻿using BLockReviewsAPI.Data;
+using BLockReviewsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLockReviewsAPI.DBService
+{
+    public interface ICategoryDBService
+    {
+        public List<Category> SelectCate(int level, string parentId);
+    }
+
+    public class CategoryDBService : ICategoryDBService
+    {
+        private BlockReviewContext _context;
+        public CategoryDBService(BlockReviewContext context)
+        {
+            _context = context;
+        }
+
+        public List<Category> SelectCate(int level, string parentId)
+        {
+            if (level > 1)
+            {
+                return _context.Categories.Where(e => e.ParentId.Equals(parentId) && e.Level == level).ToList();
+            }
+            else
+            {
+                return _context.Categories.Where(e => e.Level == level).ToList();
+            }
+        }
+    }
+}
