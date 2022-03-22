672d1b8a8da58ca0ec60e705d324c7e0fc0a8b95
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-03-22 20:25:55 +0900

    CORS

--- BLockReviewsAPI/BLockReviewsAPI/Models/UserInfo.cs
+++ BLockReviewsAPI/BLockReviewsAPI/Models/UserInfo.cs
@@ -17,7 +17,7 @@ namespace BLockReviewsAPI.Models
         public string Password { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public int? UserType { get; set; }
+        public int UserType { get; set; }
         public int? Age { get; set; }
         public string Gender { get; set; }
         public string Phone { get; set; }        
